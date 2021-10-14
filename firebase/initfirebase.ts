de11e87a3208d759221cc67a1d9fb20564d8bb15
faizan123456 <faizydhillon28@gmail.com>
2021-10-14 17:44:54 +0500

    initfirebase

--- firebase/initfirebase.ts
+++ firebase/initfirebase.ts
@@ -1,18 +1,20 @@
-
-
 import firebase from 'firebase';
 
 const clientCredentials = {
-    apiKey: "AIzaSyDFMk5OX_jVvuRHRgqC1IzHv66ebdrUIas",
-    authDomain: "fir-emulator-b0345.firebaseapp.com",
-    databaseURL: "https://fir-emulator-b0345-default-rtdb.firebaseio.com",
-    projectId: "fir-emulator-b0345",
-    storageBucket: "fir-emulator-b0345.appspot.com",
-    messagingSenderId: "633445818093",
-    appId: "1:633445818093:web:76dfbabed1e6c2b06f36e4",
-    measurementId: "G-T5TT868KMN"
+apiKey: "AIzaSyDXPajsd4TFs7Ji-njQ_M_ANalqtSejtrU",
+  authDomain: "testapp-47540.firebaseapp.com",
+  databaseURL: "https://testapp-47540-default-rtdb.firebaseio.com",
+  projectId: "testapp-47540",
+  storageBucket: "testapp-47540.appspot.com",
+  messagingSenderId: "1002297083999",
+  appId: "1:1002297083999:web:9e22c96eb39c14922773ea",
+  measurementId: "G-K5VPLE717B"
 }
 
-if (!firebase.apps.length) firebase.initializeApp(clientCredentials);
-
+if (typeof window !== 'undefined' && !firebase.apps.length) {
+    firebase.initializeApp(clientCredentials);
+    if ('measurementId' in clientCredentials) firebase.analytics();    
+} 
+export var db = firebase.firestore();
+db.useEmulator('localhost', 8080);
 export default firebase;
