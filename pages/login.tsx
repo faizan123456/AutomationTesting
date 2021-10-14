d2ceccf054e138a3e4e8e6c192f57f069017b61c
faizan123456 <faizydhillon28@gmail.com>
2021-10-14 16:27:56 +0500

    login form functional

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,28 +1,51 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
 import styles from '../styles/Authentication.module.css'
-
-
+import firebase from '../firebase/initfirebase';
+import { useState } from 'react';
+import { useRouter } from 'next/router'
 const Login: NextPage=()=> {
+  
+    const router = useRouter()
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessgae, setErrorMessage] = useState('');
+    const loginUser = () => {
+      firebase.auth().signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        console.log("userCredential", userCredential);
+        // alert("login success!");
+        console.log("login success")
+        router.push('/home')
+        // Signed in
+        // var user = userCredential.user;
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        setErrorMessage(errorMessage);
+        console.log("errorMessage", errorMessage);
+      });
+    }
     return (  
-    <div className={styles.parentContainer}>    
-
-<form className={styles.loginform} style={{width:"37vw"}}  action="/action_page.php">
-      <h1 className={styles.loginheading}>SIGN IN</h1>
-      <div className={styles.formcontainer}>
-      <div className={styles.container}>
-        <label htmlFor="mail"><strong>E-mail</strong></label>
-        <input className={styles.forminput} type="text" placeholder="Enter E-mail" name="mail" required/>
-        <label htmlFor="psw"><strong>Password</strong></label>
-        <input className={styles.forminput} type="password" placeholder="Enter Password" name="psw" required/>
+      <div className={styles.parentContainer}>    
+        <div className={styles.loginform} style={{width:"37vw"}}>
+          <h1 className={styles.loginheading}>SIGN IN</h1>
+          <div className={styles.formcontainer}>
+          <span style={{ color: "red", fontWeight: "bold" }}>{errorMessgae !== "" && errorMessgae}</span>
+          <div className={styles.container}>
+            <label htmlFor="mail"><strong>E-mail</strong></label>
+            <input className={styles.forminput} onChange={(e) => setEmail(e.target.value)} type="text" placeholder="Enter E-mail" name="mail" required/>
+            <label htmlFor="psw"><strong>Password</strong></label>
+            <input className={styles.forminput} type="password" onChange={(e) => setPassword(e.target.value)} placeholder="Enter Password" name="psw" required/>
+          </div>
+          <button className={styles.loginbtn} onClick={loginUser} type="submit"><strong>SIGN IN</strong></button>
+          <div className={styles.container}>
+          </div>
+          <span className={styles.psw}><Link href="/signup">Create Account</Link></span>
+          </div>
+        </div>
       </div>
-      <button className={styles.loginbtn} type="submit"><strong><Link href="/home"> SIGN IN</Link></strong></button>
-      <div className={styles.container}>
-      </div>
-      <span className={styles.psw}><Link href="/signup">Create Account</Link></span>
-      </div>
-    </form>
-    </div>
     )
   }
 
