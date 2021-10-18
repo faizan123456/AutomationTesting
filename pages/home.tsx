5c71e91f5a69a1ca598bd5f1e36cad7b1843a4d8
usama123456-web <oosammach@gmail.com>
2021-10-18 12:39:16 +0500

    Home page designed

--- pages/home.tsx
+++ pages/home.tsx
@@ -14,7 +14,39 @@ const Home:NextPage=()=>{
      <h4 className="mb-0 mt-2" style={{cursor:"pointer"}} onClick={()=>router.push('/login')}>Logout</h4>
  </div>
 </div>
-<h1>welcome to the home</h1>
+    <div className="" style={{display:"flex",justifyContent:"center"}}>
+
+    <h3 className="mt-4 d-flex " style={{width:"70%"}}>Create Employee</h3>
+    </div>
+<div className={styles.homeFormContainer}>
+ <div className={styles.homeInputs}>
+     <form>
+  <div className="form-group my-2">
+    <label className="mb-2" htmlFor="exampleInputEmail1">First Name</label>
+    <input type="text" className="form-control " style={{padding:"0.65rem"}} name="firstName" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter First Name"/>
+  </div>
+
+  <div className="form-group my-2">
+    <label   className="mb-2" htmlFor="exampleInputEmail1">Last Name</label>
+    <input type="text" className="form-control"  style={{padding:"0.65rem"}} name="lastName"  id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Last Name"/>
+  </div>
+
+
+  <div className="form-group my-2">
+    <label  className="mb-2" htmlFor="exampleInputEmail1">Email address</label>
+    <input type="email" className="form-control "  style={{padding:"0.65rem"}} name="email"  id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email Address"/>
+  </div>
+
+  <div className="form-group my-2">
+    <label  className="mb-2" htmlFor="exampleInputPassword1">Project Id</label>
+    <input type="text" className="form-control"  style={{padding:"0.65rem"}} name="projectId"  id="exampleInputPassword1" placeholder="Project Id"/>
+  </div>
+  <button type="submit" className="btn btn-primary my-2">Submit</button>
+</form>
+
+ </div>
+</div>
+
 </>
     )
 }
